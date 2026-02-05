7f929cf6ffe10e06a8ccf749d29a2cc61026851e
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-05 12:54:00 +0100

    provide generic functions to interact with blossom server for testing purposes

--- tests/FeatureCase.php
+++ tests/FeatureCase.php
@@ -52,43 +52,10 @@ abstract class FeatureCase extends BaseTestCase
     }
     
     static function request(string $method, string $path, ?string $body = null, array $headers = []) : array {
-        $curl = curl_init(self::RELAY_URL . $path);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        switch ($method) {
-            case 'HEAD':
-                curl_setopt($curl, CURLOPT_NOBODY, true);
-                break;
-            default:
-                break;
-                
-        }
-        
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        $raw_response = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        curl_close($curl);
-        
-        $response_headers = explode("\r\n", substr($raw_response, 0, $info['header_size']));
-        list($protocol, $code) = explode(' ', array_shift($response_headers));
-        $response_body = substr($raw_response, $info['header_size']);
-        
-        return [$protocol, $code, array_reduce($response_headers, function(array $carry, string $header) {
-            if (empty($header)) {
-                return $carry;
-            }
-            list($name, $value) = explode(":", $header, 2);
-            $carry[strtolower($name)] = trim($value, " ");
-            return $carry;
-        } , []), $response_body];
+        return Pest::request($method, self::RELAY_URL . $path, $body, $headers);
     }
     
     static function writeFile(string $content) {
-        $hash = hash('sha256', $content);
-        file_put_contents(files_directory() . DIRECTORY_SEPARATOR . $hash, $content);
-        expect(files_directory() . DIRECTORY_SEPARATOR . $hash)->toBeFile();
-        expect(file_get_contents(files_directory() . DIRECTORY_SEPARATOR . $hash))->toBe($content);
-        return $hash;
+        return Pest::writeFile(files_directory(), $content);
     }
 }
