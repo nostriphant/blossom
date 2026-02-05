cadbb41b6c1845d0d1fcb0e6c718978fee7136d5
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-05 09:15:04 +0100

    refactor tests

--- tests/Feature/BlossomTest.php
+++ tests/Feature/BlossomTest.php
@@ -9,20 +9,23 @@ beforeAll(function() {
     \nostriphant\BlossomTests\FeatureCase::relay_process();
 });
 
-it('supports BUD-01 (GET /<sha-256>)', function () {
-    $hash = $this->writeFile('Hello World!');
-    $body = $this->expectRelayResponse('/' . $hash, 200, 'text/plain');
-    expect($body)->toBe('Hello World!');
-});
+describe("BUD-01", function() {
+    it('GET /<sha-256>', function () {
+        $hash = $this->writeFile('Hello World!');
+        $body = $this->expectRelayResponse('/' . $hash, 200, 'text/plain');
+        expect($body)->toBe('Hello World!');
+    });
 
-it('supports BUD-01 (HEAD /<sha-256>)', function () {
-    $hash = $this->writeFile('Hello World!');
-    $body = $this->expectRelayResponse('/' . $hash, 200, 'text/plain', 'HEAD');
-    expect($body)->toBeEmpty();
-});
+    it('HEAD /<sha-256>', function () {
+        $hash = $this->writeFile('Hello World!');
+        $body = $this->expectRelayResponse('/' . $hash, 200, 'text/plain', 'HEAD');
+        expect($body)->toBeEmpty();
+    });
 
-it('responds with 404 when file missing', function () {
-    $body = $this->expectRelayResponse('/not-existing', 404, 'text/html');
+    it('responds with 404 when file missing', function () {
+        $body = $this->expectRelayResponse('/not-existing', 404, 'text/html');
+    });
+    
 });
 
 afterAll(function() {
