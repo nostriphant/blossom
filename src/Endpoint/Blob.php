01ee21a7af73acb80889d200cda66d13a8f4d0a3
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-06 14:35:29 +0100

    refactor

--- src/Endpoint/Blob.php
+++ src/Endpoint/Blob.php
@@ -10,10 +10,14 @@ readonly class Blob implements Endpoint {
 
     }
     
+    private function blob(string $hash) : \nostriphant\Blossom\Blob {
+        return new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $hash);
+    }
+    
     #[\Override]
     public function __invoke(callable $define) : void {
-        $define('OPTIONS', '/{hash:\w+}[.{ext:\w+}]', fn(array $attributes) => (new Blob\Options(new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash'])))());
+        $define('OPTIONS', '/{hash:\w+}[.{ext:\w+}]', fn(array $attributes) => (new Blob\Options($this->blob($attributes['hash'])))());
         
-        $define('GET', '/{hash:\w+}[.{ext:\w+}]', fn(array $attributes) => (new Blob\Get(new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash'])))());
+        $define('GET', '/{hash:\w+}[.{ext:\w+}]', fn(array $attributes) => (new Blob\Get($this->blob($attributes['hash'])))());
     }
 }
