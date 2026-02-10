297327f9ca5baece775ef19b9c19c7bda76f50d4
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-10 11:28:40 +0100

    update deps and use When from nostriphant/functional

--- src/Blob/Factory.php
+++ src/Blob/Factory.php
@@ -6,7 +6,7 @@ readonly class Factory {
     private \Closure $when_factory;
     
     public function __construct(string $path, callable $missing) {
-        $this->when_factory = fn(callable $exists) => fn(string $hash) => new \nostriphant\Blossom\When('file_exists', fn(string $blob_path) => $exists(new \nostriphant\Blossom\Blob($blob_path)), $missing)($path . DIRECTORY_SEPARATOR . $hash);
+        $this->when_factory = fn(callable $exists) => fn(string $hash) => new \nostriphant\Functional\When('file_exists', fn(string $blob_path) => $exists(new \nostriphant\Blossom\Blob($blob_path)), $missing)($path . DIRECTORY_SEPARATOR . $hash);
     }
     
     public function __invoke(callable $exists) : callable {
