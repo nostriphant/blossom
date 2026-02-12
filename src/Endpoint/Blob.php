d831879eefd61c3f06b77e2b004978912b5b2dcb
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-12 14:46:01 +0100

    refactor

--- src/Endpoint/Blob.php
+++ src/Endpoint/Blob.php
@@ -12,7 +12,7 @@ readonly class Blob implements Endpoint {
         $blob_existing = fn(callable $exists) => fn(string $blob_path) => $exists(new \nostriphant\Blossom\Blob($blob_path));
         $blob_missing = fn(callable $missing) => fn(string $blob_path) => $missing(new \nostriphant\Blossom\Blob\Missing($blob_path));
         
-        $this->blob_factory = fn(callable $exists, callable $missing) => fn(string $hash) => new \nostriphant\Functional\When(
+        $this->blob_factory = fn(callable $exists, callable $missing, string $hash) => new \nostriphant\Functional\When(
                                 'file_exists', 
                                 $blob_existing($exists), 
                                 $blob_missing($missing)
@@ -21,7 +21,7 @@ readonly class Blob implements Endpoint {
     
     #[\Override]
     public function __invoke(callable $define) : void {
-        $redefine = fn(string $method, callable $handler) => $define($method, '/{hash:\w+}[.{ext:\w+}]', fn(array $attributes) => ($this->blob_factory)($handler, fn() => ['status' => 404])($attributes['hash']));
+        $redefine = fn(string $method, callable $handler) => $define($method, '/{hash:\w+}[.{ext:\w+}]', fn(array $attributes) => ($this->blob_factory)($handler, fn() => ['status' => 404], $attributes['hash']));
         new Blob\Options($redefine);
         new Blob\Get($redefine);
     }
