8f14a0ee2121a0fa67447cb94891695b3e1497db
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-12 15:49:04 +0100

    refactor

--- src/Endpoint/Blob.php
+++ src/Endpoint/Blob.php
@@ -6,7 +6,7 @@ readonly class Blob {
     
     private \Closure $blob_factory;
     
-    public function __construct(string $path) {
+    public function __construct(private string $path) {
         $this->blob_factory = fn(callable $handler) => fn(string $hash) => new \nostriphant\Functional\When(
                                 'file_exists', 
                                 fn(string $blob_path) => $handler(new \nostriphant\Blossom\Blob($blob_path)), 
@@ -15,7 +15,7 @@ readonly class Blob {
     }
     
     public function __invoke(callable $define) : void {
-        $define('OPTIONS', fn(array $attributes) => ($this->blob_factory)(new Blob\Options())($attributes['hash']));
+        $define('OPTIONS', fn(array $attributes) => new Blob\Options()(new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash'])));
         $define('GET', fn(array $attributes) => ($this->blob_factory)(new Blob\Get())($attributes['hash']));
     }
 }
