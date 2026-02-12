0f7295d56f9aaa9919f33c0a090396a05a3e0d8f
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-12 15:52:43 +0100

    refactor

--- src/Endpoint/Blob.php
+++ src/Endpoint/Blob.php
@@ -4,18 +4,16 @@ namespace nostriphant\Blossom\Endpoint;
 
 readonly class Blob {
     
-    private \Closure $blob_factory;
     
     public function __construct(private string $path) {
-        $this->blob_factory = fn(callable $handler) => fn(string $hash) => new \nostriphant\Functional\When(
-                                'file_exists', 
-                                fn(string $blob_path) => $handler(new \nostriphant\Blossom\Blob($blob_path)), 
-                                fn(string $blob_path) => ['status' => 404],
-                        )($path . DIRECTORY_SEPARATOR . $hash);
     }
     
     public function __invoke(callable $define) : void {
         $define('OPTIONS', fn(array $attributes) => new Blob\Options()(new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash'])));
-        $define('GET', fn(array $attributes) => ($this->blob_factory)(new Blob\Get())($attributes['hash']));
+        $define('GET', fn(array $attributes) => (new \nostriphant\Functional\When(
+                                'file_exists', 
+                                fn(string $blob_path) => (new Blob\Get())(new \nostriphant\Blossom\Blob($blob_path)), 
+                                fn(string $blob_path) => ['status' => 404],
+                        ))($this->path . DIRECTORY_SEPARATOR . $attributes['hash']));
     }
 }
