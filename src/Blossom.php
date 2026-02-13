ba6c45338d0b5bb39c6634add30dee7055a8d95e
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-13 14:31:17 +0100

    add OPTIONS automatically afterwards

--- src/Blossom.php
+++ src/Blossom.php
@@ -10,12 +10,25 @@ readonly class Blossom {
        
     }
     
-    static function wrap(string $endpoint, callable $endpoint_factory) : callable {
-        return fn(callable $define) => $endpoint_factory(fn(string $method, callable $handler) => $define($method, $endpoint, fn(array $attributes, callable $stream) => $handler($attributes, $stream)()));
+    static function wrap(string $endpoint, callable $endpoint_factory, array &$endpoint_methods) : callable {
+        $endpoint_methods[$endpoint] = [];
+        return function(callable $define) use ($endpoint_factory, $endpoint, &$endpoint_methods) {
+            return $endpoint_factory(function(Method $method, callable $handler) use ($define, $endpoint, &$endpoint_methods) {
+                $endpoint_methods[$endpoint][] = $method;
+                return $define($method->name, $endpoint, fn(array $attributes, callable $stream) => $handler($attributes, $stream)());
+            });
+        };
     }
 
     public function __invoke() : \Generator {
-        yield self::wrap('/upload', new Endpoint\Upload($this->path));
-        yield self::wrap('/{hash:\w+}[.{ext:\w+}]', new Endpoint\Blob($this->path));
+        $endpoint_methods = [];
+        yield self::wrap('/upload', new Endpoint\Upload($this->path), $endpoint_methods);
+        yield self::wrap('/{hash:\w+}[.{ext:\w+}]', new Endpoint\Blob($this->path), $endpoint_methods);
+        foreach ($endpoint_methods as $endpoint => $methods) {
+            if (in_array(Method::GET, $methods) && in_array(Method::HEAD, $methods) === false) {
+                $methods[] = Method::HEAD;
+            }
+            yield fn(callable $define) => $define('OPTIONS', $endpoint, new Endpoint\Options(...$methods));
+        }
     }
 }
--- src/Endpoint/Blob.php
+++ src/Endpoint/Blob.php
@@ -9,7 +9,6 @@ readonly class Blob {
     }
     
     public function __invoke(callable $define) : void {
-        $define('OPTIONS', fn() => new Options(\nostriphant\Blossom\Method::GET, \nostriphant\Blossom\Method::HEAD));
-        $define('GET', fn(array $attributes) => new Blob\Get(new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash'])));
+        $define(\nostriphant\Blossom\Method::GET, fn(array $attributes) => new Blob\Get(new \nostriphant\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash'])));
     }
 }
