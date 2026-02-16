7a5146cd18173f6612905a6957febe7ce4644d81
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-16 13:22:12 +0100

    refactor

--- src/Endpoint/Blob/Factory.php
+++ src/Endpoint/Blob/Factory.php
@@ -12,14 +12,14 @@ class Factory {
     }
     
     public function __invoke(string $class) : \nostriphant\Blossom\Endpoint\Action\Factory {
-        return new class($class, $this->blob_factory) implements \nostriphant\Blossom\Endpoint\Action\Factory  {
-            private \Closure $blob_factory;
-            public function __construct(private string $class, callable $blob_factory) {
-                $this->blob_factory = \Closure::fromCallable($blob_factory);
+        return new class($class, fn(\nostriphant\Blossom\HTTP\ServerRequest $request) => [($this->blob_factory)($request->attributes['hash'])]) implements \nostriphant\Blossom\Endpoint\Action\Factory  {
+            private \Closure $translate_request;
+            public function __construct(private string $class, callable $translate_request) {
+                $this->translate_request = \Closure::fromCallable($translate_request);
             }
 
             public function __invoke(\nostriphant\Blossom\HTTP\ServerRequest $request) : \nostriphant\Blossom\Endpoint\Action {
-                return new ($this->class)(($this->blob_factory)($request->attributes['hash']));
+                return new ($this->class)(...call_user_func($this->translate_request, $request));
             }
         };
     }
