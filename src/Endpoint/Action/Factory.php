0c87674dd16e8fa7b6161cbb060421958d10eb36
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-16 13:24:06 +0100

    refactor

--- src/Endpoint/Action/Factory.php
+++ src/Endpoint/Action/Factory.php
@@ -2,8 +2,13 @@
 
 namespace nostriphant\Blossom\Endpoint\Action;
 
-interface Factory {
-    
-    public function __invoke(\nostriphant\Blossom\HTTP\ServerRequest $request) : \nostriphant\Blossom\Endpoint\Action;
-    
+class Factory {
+    private \Closure $translate_request;
+    public function __construct(private string $class, callable $translate_request) {
+        $this->translate_request = \Closure::fromCallable($translate_request);
+    }
+
+    public function __invoke(\nostriphant\Blossom\HTTP\ServerRequest $request) : \nostriphant\Blossom\Endpoint\Action {
+        return new ($this->class)(...call_user_func($this->translate_request, $request));
+    }
 }
--- src/Endpoint/Blob/Factory.php
+++ src/Endpoint/Blob/Factory.php
@@ -12,15 +12,6 @@ class Factory {
     }
     
     public function __invoke(string $class) : \nostriphant\Blossom\Endpoint\Action\Factory {
-        return new class($class, fn(\nostriphant\Blossom\HTTP\ServerRequest $request) => [($this->blob_factory)($request->attributes['hash'])]) implements \nostriphant\Blossom\Endpoint\Action\Factory  {
-            private \Closure $translate_request;
-            public function __construct(private string $class, callable $translate_request) {
-                $this->translate_request = \Closure::fromCallable($translate_request);
-            }
-
-            public function __invoke(\nostriphant\Blossom\HTTP\ServerRequest $request) : \nostriphant\Blossom\Endpoint\Action {
-                return new ($this->class)(...call_user_func($this->translate_request, $request));
-            }
-        };
+        return new \nostriphant\Blossom\Endpoint\Action\Factory($class, fn(\nostriphant\Blossom\HTTP\ServerRequest $request) => [($this->blob_factory)($request->attributes['hash'])]);
     }
 }
