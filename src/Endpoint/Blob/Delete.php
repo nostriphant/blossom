1cd93245aa72ee2e3f035f06c17f843750f459a8
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-16 19:29:12 +0100

    check for x, delete tag in authorization event

--- src/Endpoint/Blob/Delete.php
+++ src/Endpoint/Blob/Delete.php
@@ -7,6 +7,11 @@ readonly class Delete implements \nostriphant\Blossom\Endpoint\Action {
     public function __construct(private \nostriphant\Blossom\Blob $blob) {}
     
     public function authorize(\nostriphant\NIP01\Event $authorization_event) : bool {
+        if (\nostriphant\NIP01\Event::hasTagValue($authorization_event, 'x', $this->blob->sha256) === false) {
+            return false;
+        } elseif (\nostriphant\NIP01\Event::hasTagValue($authorization_event, 't', 'delete') === false) {
+            return false;
+        }
         return true;
     }
     
