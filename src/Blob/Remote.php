8937592eb299f235631655811b8c30ed608616c5
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-23 13:19:44 +0100

    refactor

--- src/Blob/Remote.php
+++ src/Blob/Remote.php
@@ -5,10 +5,7 @@ namespace nostriphant\Blossom\Blob;
 
 class Remote {
     
-    private \Closure $unsupported_media_types;
-    
-    public function __construct(private \nostriphant\Blossom\VFS\Directory $directory, callable $unsupported_media_types) {
-        $this->unsupported_media_types = \Closure::fromCallable($unsupported_media_types);
+    public function __construct(private \nostriphant\Blossom\VFS\Directory $directory) {
     }
     
     public function __invoke(string $pubkey_hex, $handle_remote, string $hash): \nostriphant\Blossom\Blob {
