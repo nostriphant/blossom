02e8cf27fe1f95173089263e42dc3f049a8e9527
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-24 08:35:03 +0100

    update deps and refactoring

--- src/Endpoint/Upload/Put.php
+++ src/Endpoint/Upload/Put.php
@@ -11,11 +11,10 @@ readonly class Put implements \nostriphant\Blossom\Endpoint\Action {
     }
     
     public function authorize(\nostriphant\NIP01\Event $authorization_event, array $additional_headers, callable $action, callable $unauthorized) : array {
-        return $action(fn(string $pubkey_hex) => ($this->upload_authorized)(
-                $pubkey_hex, 
+        return $action(\nostriphant\Functional\Partial::right($this->upload_authorized,
                 $additional_headers['CONTENT_LENGTH'] ?? -1, 
                 $additional_headers['CONTENT_TYPE'] ?? "application/octet-stream", 
-                fn(string $pubkey_hex) => ($this->blob)($pubkey_hex, $this->stream, \nostriphant\NIP01\Event::extractTagValues($authorization_event, 'x')[0][0]),
+                \nostriphant\Functional\Partial::right($this->blob, $this->stream, \nostriphant\NIP01\Event::extractTagValues($authorization_event, 'x')[0][0]),
                 $unauthorized
         ));
     }
