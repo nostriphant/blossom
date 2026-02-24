94670755701922f8326b170f664229567fee78e2
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-24 09:57:33 +0100

    refactor

--- src/Endpoint/Upload/Put.php
+++ src/Endpoint/Upload/Put.php
@@ -2,6 +2,8 @@
 
 namespace nostriphant\Blossom\Endpoint\Upload;
 
+use \nostriphant\Functional\Partial;
+
 readonly class Put implements \nostriphant\Blossom\Endpoint\Action {
 
     private \Closure $upload_authorized;
@@ -11,10 +13,10 @@ readonly class Put implements \nostriphant\Blossom\Endpoint\Action {
     }
     
     public function authorize(\nostriphant\NIP01\Event $authorization_event, array $additional_headers, callable $action, callable $unauthorized) : array {
-        return $action(\nostriphant\Functional\Partial::right($this->upload_authorized,
+        return $action(Partial::right($this->upload_authorized,
                 $additional_headers['CONTENT_LENGTH'] ?? -1, 
                 $additional_headers['CONTENT_TYPE'] ?? "application/octet-stream", 
-                \nostriphant\Functional\Partial::right($this->blob, $this->stream, \nostriphant\NIP01\Event::extractTagValues($authorization_event, 'x')[0][0]),
+                Partial::right($this->blob, $this->stream, \nostriphant\NIP01\Event::extractTagValues($authorization_event, 'x')[0][0]),
                 $unauthorized
         ));
     }
